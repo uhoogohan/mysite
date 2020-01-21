e2c5c0ece6e9c9491c620364f4b5ca4f0a08e3d8
uhoo <i-miyagi@xj9.so-net.ne.jp>
2020-01-21 21:15:32 +0900

    modified paginator

--- myapp/templatetags/extras.py
+++ myapp/templatetags/extras.py
@@ -2,7 +2,7 @@ from django import template
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
-def set_page(context, page):
+def page_link(context, page):
     query = context['request'].GET.copy()
     query['page'] = page
     return query.urlencode()
